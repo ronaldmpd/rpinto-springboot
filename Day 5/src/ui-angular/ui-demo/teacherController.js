63f27959e3e8ed94f8fcbe0562e2088baeafe4f8
Ronald Pinto <ronald.pinto.d@gmail.com>
2018-05-04 20:17:28 -0400

    add zip

---
+++ Day 5/src/ui-angular/ui-demo/teacherController.js
@@ -0,0 +1,49 @@
+$(function () {
+    $.getJSON("http://localhost:9090/teachers", function(data){
+        var quickGrid = $("#myGrid").quickGrid({
+        data: data,
+        onrowdelete: function ( rownum, rowdata) {
+            console.log("the row to delete is ", rowdata);
+            console.log("the row number ", rownum);
+            $.ajax({
+                type: "DELETE",
+                url: "http://localhost:9090/teachers",
+                data:  JSON.stringify(rowdata),
+                success: function(){
+                    console.log("DONE DELETE")
+                },
+                contentType: 'application/json'
+            });
+         }
+
+        })
+        .on('qgrd:updaterow', function(event, rowData){
+            //console.log("update data", rowData);
+            $.ajax({
+                type: "PATCH",
+                url: "http://localhost:9090/teachers",
+                data:  JSON.stringify(rowData),
+                success: function(){
+                    console.log("DONE UPDATE")
+                },
+                contentType: 'application/json'
+            });
+            
+            console.log("The updated data Is:", rowData)
+        })
+        .on('qgrd:addrow', function(event, rowData){
+            // do something 
+           //console.log("add data", rowData);
+           $.ajax({
+                type: "POST",
+                url: "http://localhost:9090/teachers",
+                data:  JSON.stringify(rowData),
+                success: function(){
+                    console.log("DONE POST")
+                },
+                contentType: 'application/json'
+            });
+            console.log("The new Data is", rowData)
+        })
+    });
+});
