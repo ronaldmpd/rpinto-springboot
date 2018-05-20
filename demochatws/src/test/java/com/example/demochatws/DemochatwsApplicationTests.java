bad96a04b8bc3b65cbe59e72d1add8b443a717f4
Ronald Pinto <ronald.pinto.d@gmail.com>
2018-05-20 11:10:55 -0400

    Add demo chat web services

---
+++ demochatws/src/test/java/com/example/demochatws/DemochatwsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demochatws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemochatwsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
